5c441bc792a2fbd6a9970280fd257ef681b74e18
travian1988 <igorallin11@gmail.com>
2019-06-23 21:38:13 +0300

    init

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var number float64;
+
+func isPrime(devider float64) string {
+	if devider > number / 2 {
+		return "Tris number is prime"
+	}
+
+	divisionResult := number / devider;
+
+	if divisionResult == math.Trunc(divisionResult) {
+		return "Tris number is not prime"
+	}
+
+	return isPrime(devider + 1)
+}
+
+func main()  {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+
+	//Maybe there is exist better way to delete those symbols from the end of command line string?
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	//Parse input string to float64
+	input, err := strconv.ParseFloat(text, 64)
+
+	if err != nil{
+		fmt.Println(err)
+
+		return
+	}
+
+	number = input
+
+	fmt.Println(isPrime(2))
+}
